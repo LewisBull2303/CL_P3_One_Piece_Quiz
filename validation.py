114c1f9170b2cd9bc8767d962a84af973a658d88
Lewis Bull <lewisbull2303@gmail.com>
2025-03-05 22:21:12 +0000

    Created a function to get the players email and validate the email

--- validation.py
+++ validation.py
@@ -2,8 +2,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 from email_validator import validate_email, EmailNotValidError
 
-
-
 #  Scope and constant variables for google api and sheets
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,7 +18,20 @@ SHEET = GSPREAD_CLIENT.open('scoreboard')
 SCOREBOARD = SHEET.worksheet('scores')
 PLAYER_SHEET = SHEET.worksheet('players')
 
+email = ''
+
 def get_emails():
     """
     This function will get the players email to store in the players spreadsheet
     """
+    global email
+    email = input("What is your email address?:\n")
+
+    try:
+        validate_email(email)
+        return True
+    except EmailNotValidError as e:
+        print(str(e))
+        print("Sorry this email is not valid, Please Try again!\n")
+        get_emails()
+        return False
