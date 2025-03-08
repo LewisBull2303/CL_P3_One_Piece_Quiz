07c4e6da04fe08701efa379ec7dafab0c4e644a7
Lewis Bull <lewisbull2303@gmail.com>
2025-03-08 17:02:46 +0000

    Fixed the unittests

--- test_validation.py
+++ test_validation.py
@@ -1,31 +1,36 @@
 import unittest
 import validation as val
+from email_validator import EmailSyntaxError
 
 class TestEmailValidation(unittest.TestCase):
     """
-    This class will verify the users email
+    This class will verify the user's email
     """
-
     def test_validate_email(self):
-        self.assertTrue(val.validate_email("correct_email@gmail.com"), True)
-
+        self.assertTrue(val.validate_email("correct_email@gmail.com"))
+    
     def test_validate_wrong_email(self):
-        self.assertEqual(val.validate_email("correct_email@gmail"), None)
-        self.assertEqual(val.validate_email("correct_emailgmail.com"), None)
+        self.assertFalse(val.validate_email("correct_email@gmail"))
+        self.assertFalse(val.validate_email("correct_emailgmail.com"))
+
 
 class TestUserLogin(unittest.TestCase):
     """
     Verification of the inputs for the user login
     """
-
     def test_log_in(self):
-        self.assertTrue(val.player_login([val.name, val.email]), True)
-        self.assertEqual(val.player_login(1), None)
-    
+        self.assertTrue(val.player_login("user@gmail.com"))
+        self.assertFalse(val.player_login(""))
+
+
 class TestUserRegistration(unittest.TestCase):
     """
     Verification of the inputs for the user registration
     """
     def test_register_user(self):
-        self.assertTrue(val.register_user[val.name, val.email], True)
-        self.assertEqual(val.register_user(1), None)
+        self.assertTrue(val.register_user("new_user", "email@gmail.com"))
+        self.assertFalse(val.register_user("", ""))
+
+
+if __name__ == "__main__":
+    unittest.main()
