ba9e5e08f764e770fa6d1b3201ffb8a26cdb1d96
Lewis Bull <lewisbull2303@gmail.com>
2025-03-08 16:53:24 +0000

    Added test for the user login and registration

--- test_validation.py
+++ test_validation.py
@@ -13,4 +13,19 @@ class TestEmailValidation(unittest.TestCase):
         self.assertEqual(val.validate_email("correct_email@gmail"), None)
         self.assertEqual(val.validate_email("correct_emailgmail.com"), None)
 
+class TestUserLogin(unittest.TestCase):
+    """
+    Verification of the inputs for the user login
+    """
+
+    def test_log_in(self):
+        self.assertTrue(val.player_login([val.name, val.email]), True)
+        self.assertEqual(val.player_login(1), None)
     
+class TestUserRegistration(unittest.TestCase):
+    """
+    Verification of the inputs for the user registration
+    """
+    def test_register_user(self):
+        self.assertTrue(val.register_user[val.name, val.email], True)
+        self.assertEqual(val.register_user(1), None)
